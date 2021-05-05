16c2a8ac72e89f1caded69ae931c46c37b887f57
gregorio92 <gregorio.espindolaa@gmail.com>
2021-05-05 20:57:50 -0300

    instaciando-bloques

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,13 +1,17 @@
 extends TextureButton
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var esena = load("res://scenes/Blocks/Red/BlockRed.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func generate_block():
+	var node = esena.instance()
+	add_child(node)
+	pass
+
+
+func _on_RedGenerator_button_down():
+	generate_block()
+	pass # Replace with function body.
