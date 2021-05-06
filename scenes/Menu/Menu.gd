43d05a88cd5150a240eda0924b8443216f0eb97d
gregorio92 <gregorio.espindolaa@gmail.com>
2021-05-05 23:02:57 -0300

    conectando-meno-con-instacia

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,18 +8,23 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	$RedGenerator.connect("button_down",self,"count_new_instance")
+	$VioletGenerator.connect("button_down",self,"count_new_instance")
 	pass
 
 func count_new_instance():
 	instancesCount += 1
+	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
 	pass
 	
 	
 	
 	
 func _on_VioletGenerator_button_down():
+	count_new_instance()
 	pass # Replace with function body.
 
 
 func _on_RedGenerator_button_down():
+	count_new_instance()
 	pass # Replace with function body.
